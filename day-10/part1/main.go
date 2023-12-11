32ea3f0b4d9606e0b6ca9db09599f9808c8ff68b
Daniel Rodriguez <daniel@danielfrg.com>
2023-12-10 22:10:20 -0600

    Day 10 part 1

--- day-10/part1/main.go
+++ day-10/part1/main.go
@@ -3,45 +3,187 @@ package main
 import (
 	"advent-of-code-2023/utils"
 	"fmt"
-	"os"
-	"slices"
 	"strings"
 
 	"github.com/dominikbraun/graph"
-	"github.com/dominikbraun/graph/draw"
 )
 
 func main() {
-	// input, starting := readInput("input.txt")
-	// initialShape := "L" // Saw manually
+	// input, starting := readInput("input-test.txt")
+	// initialShape := "F" // Saw manually
 
-	input, starting := readInput("input-test.txt")
-	initialShape := "F" // Saw manually
+	// input, starting := readInput("input-test-2.txt")
+	// initialShape := "F" // Saw manually
+
+	input, starting := readInput("input.txt")
+	initialShape := "L" // Saw manually
 
 	input[starting[0]][starting[1]] = initialShape
 
-	fmt.Println(input)
+	// fmt.Println(input)
 
 	matrix := make([][]int, len(input))
-	g := graph.New(graph.IntHash, graph.PreventCycles())
+	// g := graph.New(graph.IntHash, graph.PreventCycles())
 	for i := range matrix {
 		matrix[i] = make([]int, len(input[0]))
 
 		for j := 0; j < len(input[0]); j++ {
-			myID := i*len(input[0]) + j
-			_ = g.AddVertex(myID + 1)
-			// fmt.Println(myID)
+			matrix[i][j] = 0
+		}
+		// for j := 0; j < len(input[0]); j++ {
+		// myID := i*len(input[0]) + j
+		// _ = g.AddVertex(myID + 1)
+		// }
+	}
+
+	matrix[starting[0]][starting[1]] = -1
+
+	// link(input, g, &matrix, starting)
+
+	// file, _ := os.Create("./mygraph.gv")
+	// _ = draw.DOT(g, file)
+
+	iters := make([][]int, 0)
+	iters = append(iters, starting)
+
+	c := 0
+	for {
+		nIters := make([][]int, 0)
+		fmt.Println(c, iters)
+		for _, pos := range iters {
+			n := iter(input, &matrix, pos)
+			// fmt.Println(pos, n)
+			nIters = append(nIters, n...)
+		}
+
+		if len(nIters) == 0 {
+			break
+		}
+		iters = nIters
+		c++
+		// if c == 4 {
+		// 	fmt.Println(iters)
+		// 	break
+		// }
+	}
+
+	for i := 0; i < len(matrix); i++ {
+		fmt.Println(matrix[i])
+	}
+
+	fmt.Println("----------")
+	fmt.Println(c)
+}
+
+func iter(input [][]string, matrix *[][]int, pos []int) [][]int {
+	X := pos[0]
+	Y := pos[1]
+	xLim := len(input[0])
+	yLim := len(input)
+	shape := input[X][Y]
+	// fmt.Println(shape)
+
+	value := (*matrix)[X][Y]
+	if value == -1 {
+		value = 0
+	}
+	next := make([][]int, 0)
+
+	switch shape {
+	case "|":
+		// North
+		if X-1 > 0 {
+			if (*matrix)[X-1][Y] == 0 {
+				(*matrix)[X-1][Y] = value + 1
+				next = append(next, []int{X - 1, Y})
+			}
+		}
+		// South
+		if X+1 < yLim {
+			if (*matrix)[X+1][Y] == 0 {
+				(*matrix)[X+1][Y] = value + 1
+				next = append(next, []int{X + 1, Y})
+			}
+		}
+	case "-":
+		// West
+		if Y-1 >= 0 {
+			if (*matrix)[X][Y-1] == 0 {
+				(*matrix)[X][Y-1] = value + 1
+				next = append(next, []int{X, Y - 1})
+			}
+		}
+		// East
+		if Y+1 < xLim {
+			if (*matrix)[X][Y+1] == 0 {
+				(*matrix)[X][Y+1] = value + 1
+				next = append(next, []int{X, Y + 1})
+			}
+		}
+	case "L":
+		// North
+		if X-1 > 0 {
+			if (*matrix)[X-1][Y] == 0 {
+				(*matrix)[X-1][Y] = value + 1
+				next = append(next, []int{X - 1, Y})
+			}
+		}
+		// East
+		if Y+1 < xLim {
+			if (*matrix)[X][Y+1] == 0 {
+				(*matrix)[X][Y+1] = value + 1
+				next = append(next, []int{X, Y + 1})
+			}
+		}
+	case "J":
+		// North
+		if X-1 >= 0 {
+			if (*matrix)[X-1][Y] == 0 {
+				(*matrix)[X-1][Y] = value + 1
+				next = append(next, []int{X - 1, Y})
+			}
+		}
+		// West
+		if Y-1 >= 0 {
+			if (*matrix)[X][Y-1] == 0 {
+				(*matrix)[X][Y-1] = value + 1
+				next = append(next, []int{X, Y - 1})
+			}
+		}
+	case "7":
+		// South
+		if X+1 < yLim {
+			if (*matrix)[X+1][Y] == 0 {
+				(*matrix)[X+1][Y] = value + 1
+				next = append(next, []int{X + 1, Y})
+			}
+		}
+		// West
+		if Y-1 >= 0 {
+			if (*matrix)[X][Y-1] == 0 {
+				(*matrix)[X][Y-1] = value + 1
+				next = append(next, []int{X, Y - 1})
+			}
+		}
+	case "F":
+		// South
+		if X+1 < yLim {
+			if (*matrix)[X+1][Y] == 0 {
+				(*matrix)[X+1][Y] = value + 1
+				next = append(next, []int{X + 1, Y})
+			}
+		}
+		// East
+		if Y+1 < xLim {
+			if (*matrix)[X][Y+1] == 0 {
+				(*matrix)[X][Y+1] = value + 1
+				next = append(next, []int{X, Y + 1})
+			}
 		}
 	}
 
-	link(input, g, &matrix, starting)
+	return next
 
-	file, _ := os.Create("./mygraph.gv")
-	_ = draw.DOT(g, file)
-
-	// v, _ := g.Vertex(startID)
-	// fmt.Println(v)
-	fmt.Println(g.Edges())
 }
 
 func link(input [][]string, g graph.Graph[int, int], matrix *[][]int, pos []int) {
@@ -68,7 +210,7 @@ func link(input [][]string, g graph.Graph[int, int], matrix *[][]int, pos []int)
 	switch shape {
 	case "|":
 		// North
-		if X-1 > 0 {
+		if X-1 >= 0 {
 			northID := (X-1)*xLim + Y + 1
 			fmt.Println("|", "north", northID)
 			_ = g.AddEdge(myID, northID)
@@ -169,8 +311,8 @@ func readInput(fname string) ([][]string, []int) {
 		cols := strings.Split(line, "")
 		rows = append(rows, cols)
 
-		idx := slices.IndexFunc(cols, func(c string) bool { return c == "S" })
-		if idx > 0 {
+		idx := strings.Index(line, "S")
+		if idx >= 0 {
 			starting[0] = i
 			starting[1] = idx
 		}
