40b07e5bccdb07a14e2403630369a13e069ae4fc
Daniel Rodriguez <daniel@danielfrg.com>
2023-12-11 14:25:21 -0600

    Day 11 part 2

--- day-11/part2/main.go
+++ day-11/part2/main.go
@@ -6,7 +6,6 @@ import (
 	"log"
 	"math"
 	"os"
-	"slices"
 	"strings"
 )
 
@@ -16,9 +15,6 @@ func main() {
 	// fmt.Println("----------")
 
 	input := readInput("input.txt")
-
-	input = expand(input, 2)
-	// printMat(input)
 
 	indices := make(map[int][]int, 0)
 
@@ -32,8 +28,11 @@ func main() {
 		}
 	}
 
-	// fmt.Println(indices)
+	spacesX, spacesY := expand(input)
 
+	fmt.Println(spacesX, spacesY)
+
+	rate := 1000000
 	distances := make([]int, 0)
 
 	for i := 1; i < len(indices)+1; i++ {
@@ -41,9 +40,16 @@ func main() {
 		// fmt.Println(i, p1)
 		for j := i; j < len(indices)+1; j++ {
 			p2 := indices[j]
-			distance := math.Abs(float64(p2[1]-p1[1])) + math.Abs(float64(p2[0]-p1[0]))
+			distance := int(math.Abs(float64(p2[1]-p1[1])) + math.Abs(float64(p2[0]-p1[0])))
+
+			extraX := countBetween(spacesX, p1[0], p2[0])
+			extraY := countBetween(spacesY, p1[1], p2[1])
+
+			distance = distance + extraX*(rate-1)
+			distance = distance + extraY*(rate-1)
+
 			// fmt.Println(i, j, distance)
-			distances = append(distances, int(distance))
+			distances = append(distances, distance)
 		}
 	}
 
@@ -51,6 +57,58 @@ func main() {
 
 	fmt.Println("----------")
 	fmt.Println(sum(distances))
+}
+
+func countBetween(arr []int, p1 int, p2 int) int {
+	low := 0
+	high := 0
+	if p1 < p2 {
+		low = p1
+		high = p2
+	} else {
+		low = p2
+		high = p1
+	}
+
+	c := 0
+	for _, v := range arr {
+		if v > low && v < high {
+			c++
+		}
+	}
+
+	return c
+}
+
+func expand(in [][]string) ([]int, []int) {
+	// Expand rows
+	targetX := make([]int, 0)
+
+	for i := 0; i < len(in); i++ {
+		line := in[i]
+		test := countArr(line, ".")
+		if test == len(line) {
+			targetX = append(targetX, i)
+		}
+	}
+
+	// Expand cols
+
+	targetY := make([]int, 0)
+	for i := 0; i < len(in[0]); i++ {
+		include := true
+		for j := 0; j < len(in); j++ {
+			if in[j][i] != "." {
+				include = false
+				break
+			}
+		}
+		if include {
+			targetY = append(targetY, i)
+		}
+	}
+
+	return targetX, targetY
 }
 
 func sum(arr []int) int {
@@ -61,56 +119,15 @@ func sum(arr []int) int {
 	return s
 }
 
-func expand(in [][]string, rate int) [][]string {
-	// Expand rows
-	ret := make([][]string, 0)
-
-	for i := 0; i < len(in); i++ {
-		line := in[i]
-		test := countArr(line, ".")
-		if test == len(line) {
-			for e := 0; e < rate; e++ {
-				ret = append(ret, line)
-			}
-		} else {
-			ret = append(ret, line)
-		}
+func insert(arr []string, index int, val string, repeat int) []string {
+	if len(arr) == index {
+		return append(arr, val)
 	}
-
-	// Expand cols
-
-	target := make([]int, 0)
-	for i := 0; i < len(in[0]); i++ {
-		include := true
-		for j := 0; j < len(in); j++ {
-			if in[j][i] != "." {
-				include = false
-				break
-			}
-		}
-		if include {
-			target = append(target, i)
-		}
+	arr = append(arr[:index+repeat], arr[index:]...)
+	for r := 0; r < repeat; r++ {
+		arr[index+r] = val
 	}
-
-	// fmt.Println(target)
-
-	for i := 0; i < len(ret); i++ {
-		line := make([]string, 0)
-		for j := 0; j < len(ret[i]); j++ {
-			val := ret[i][j]
-			if slices.Contains(target, j) {
-				for r := 0; r < rate; r++ {
-					line = append(line, ".")
-				}
-			} else {
-				line = append(line, val)
-			}
-		}
-		ret[i] = line
-	}
-
-	return ret
+	return arr
 }
 
 func countArr(in []string, char string) int {
